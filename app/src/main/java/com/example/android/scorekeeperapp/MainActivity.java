f3a96fdbb5df98d797b9b53f8d18f2035240a3f2
honfong <15714095+jerrychong25@users.noreply.github.com>
2018-08-21 13:51:00 +0800

    Modified button functionality

--- app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeperapp/MainActivity.java
@@ -9,45 +9,77 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int foulTeamA = 0;
+    int foulTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        displayScoreForTeamA(scoreTeamA);
+        displayScoreForTeamB(scoreTeamB);
     }
 
     public void resetScore(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        foulTeamA = 0;
+        foulTeamB = 0;
+        displayScoreForTeamA(scoreTeamA);
+        displayScoreForTeamB(scoreTeamB);
+        displayFoulForTeamA(foulTeamA);
+        displayFoulForTeamB(foulTeamB);
     }
 
     public void plusTeamAOnePoint(View view) {
         scoreTeamA += 1;
-        displayForTeamA(scoreTeamA);
+        displayScoreForTeamA(scoreTeamA);
+    }
+
+    public void plusTeamAOneFoul(View view) {
+        foulTeamA += 1;
+        displayFoulForTeamA(foulTeamA);
     }
 
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
+    public void displayScoreForTeamA(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given foul for Team A.
+     */
+    public void displayFoulForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_foul);
         scoreView.setText(String.valueOf(score));
     }
 
     public void plusTeamBOnePoint(View view) {
         scoreTeamB += 1;
-        displayForTeamB(scoreTeamB);
+        displayScoreForTeamB(scoreTeamB);
+    }
+
+    public void plusTeamBOneFoul(View view) {
+        foulTeamB += 1;
+        displayFoulForTeamB(foulTeamB);
     }
 
     /**
      * Displays the given score for Team B.
      */
-    public void displayForTeamB(int score) {
+    public void displayScoreForTeamB(int score) {
         TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given foul for Team B.
+     */
+    public void displayFoulForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_foul);
         scoreView.setText(String.valueOf(score));
     }
 }
